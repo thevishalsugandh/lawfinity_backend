77262c44cecca57163048bd2bda77b0b4865e400
thevishalsugandh <106393990+thevishalsugandh@users.noreply.github.com>
2024-07-11 23:58:05 +0530

    Update

--- lawfinity_backend.py
+++ lawfinity_backend.py
@@ -1,38 +1,20 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from langchain_community.embeddings import HuggingFaceEmbeddings  # Updated import
-from langchain_community.llms import HuggingFaceEndpoint  # Updated import
-from langchain_community.vectorstores import MongoDBAtlasVectorSearch  # Updated import
-from langchain_core.callbacks.manager import CallbackManager  # Updated import
-from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  # Updated import
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Import MongoDB connection details from environment variables
-MONGODB_URI = os.getenv('MONGODB_URI')
-DB_NAME = os.getenv('DATABASE_NAME')
-COLLECTION_NAME = os.getenv('COLLECTION_NAME')
-ATLAS_VECTOR_SEARCH_INDEX_NAME = os.getenv('ATLAS_VECTOR_SEARCH_INDEX_NAME')
-
-# Validate environment variables
-if not all([MONGODB_URI, DB_NAME, COLLECTION_NAME, ATLAS_VECTOR_SEARCH_INDEX_NAME]):
-    raise ValueError("All environment variables must be set.")
+# Import MongoDB connection details
+MONGODB_URI = "mongodb+srv://law:law123456@clusterone.irlnjku.mongodb.net/?retryWrites=true&w=majority&appName=clusterone"
+DB_NAME = "lawdb"
+COLLECTION_NAME = "bankruptcy"
+ATLAS_VECTOR_SEARCH_INDEX_NAME = "index_bankruptcy"
 
 # Create FastAPI app
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Get embedding model
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
@@ -46,20 +28,27 @@ vector_search = MongoDBAtlasVectorSearch.from_connection_string(
 )
 
 # Load LLM model
-llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-    max_length=128,
-    temperature=0.01,
-    token="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"
-)
+llm = LlamaCpp(
+        model_path="./mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+        temperature=0.75,
+        max_tokens=2000,
+        top_p=0.1,
+        n_ctx=8192,
+        n_batch=512,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+        handle_parsing_errors=True,
+        stream=True,
+        verbose=True,
+    )
 
-# Define QA retriever
+# Get QA chain
 qa_retriever = vector_search.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": 5}
+   search_type="similarity",
+   search_kwargs={
+       "k": 5
+   }
 )
 
-# Define prompt template
 prompt_template = """
 Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -69,19 +58,12 @@ If you don't know the answer, just say that you don't know, don't try to make up
 Question: {question}
 """
 PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
+   template=prompt_template, input_variables=["context", "question"]
 )
 
-# Define QA chain
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=qa_retriever,
-    return_source_documents=True,
-    chain_type_kwargs={"prompt": PROMPT}
-)
+qa = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", retriever=qa_retriever, return_source_documents=True, chain_type_kwargs={"prompt": PROMPT})
 
-# Process LLM response
+# Cite sources
 def process_llm_response(llm_response):
     print(llm_response['result'])
     print('\n\nSources:')
@@ -91,23 +73,11 @@ def process_llm_response(llm_response):
         sources.append(source.metadata['source'])
     return llm_response['result'], sources
 
-# Define home endpoint
-@app.get("/")
-async def home():
-    return {"response": "Hello World"}
-
-# Define request model
-class QuestionRequest(BaseModel):
-    question: str
-
 # Define endpoint to get response from language model
-@app.post("/")
-async def ask_question(request: QuestionRequest):
-    result = qa({"query": request.question})
-    answer, sources = process_llm_response(result)
-    return {"answer": answer, "sources": sources}
+@app.get("/ask")
+async def ask_question(question: str):
+    result = qa({"query": question})
+    response, sources = process_llm_response(result)
+    return {"response": response, "sources": sources}
 
-# Main entry point
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("lawfinity_backend:app", host="0.0.0.0", port=10000)
+
